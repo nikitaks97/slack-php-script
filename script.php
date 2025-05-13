d9e783425744f126e824c54bd162f2d39d25fbb0
Nikita Kashyap <161322388+nikitaks97@users.noreply.github.com>
2025-05-13 09:23:25 +0200

    Update script.php

--- script.php
+++ script.php
@@ -3,7 +3,7 @@ require_once 'vendor/rmccue/requests/src/Autoload.php';
 WpOrg\Requests\Autoload::register();
 
 $response =  WpOrg\Requests\Requests::post(
-  'https://hooks.slack.com/services/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',
+  'https://hooks.slack.com/services/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',
   array(
     'Content-Type' => 'application/json'
   ),
